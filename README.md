d50a125d6eecc55a464a4b8bf758afda9058b945
moritzzoepffel <moritzzoepffel@gmail.com>
2023-03-15 16:19:29 +0100

    Initial commit

---
+++ README.md
@@ -0,0 +1,1 @@
+# RealEstateInvestmentAnalyzer
---
+++ Raspberry/raspiScript.py
@@ -0,0 +1,36 @@
+# Script that gets data from an Api and saves it to MongoDB Atlas Database
+
+import requests
+import pymongo
+import Credentials.credentials as credentials
+
+
+# Get data from Api
+def get_data(url):
+    payload = {}
+    headers = {
+        'User-Agent': 'Mozilla/5.0 (Windows NT 6.1; WOW64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/56.0.2924.76 Safari/537.36',
+        "Upgrade-Insecure-Requests": "1", "DNT": "1",
+        "Accept": "text/html,application/xhtml+xml,application/xml;q=0.9,*/*;q=0.8",
+        "Accept-Language": "en-US,en;q=0.5", "Accept-Encoding": "gzip, deflate"}
+
+    response = requests.request("GET", url, headers=headers, data=payload)
+    return response.json()
+
+
+# Save data to MongoDB Atlas Database
+def save_data(data, dbname):
+    client = pymongo.MongoClient(
+        f"mongodb+srv://moritzzoepffel:{credentials.MONGO_DB_PW}@{credentials.MONGO_DB_DB}/?retryWrites=true&w=majority")
+    db = client.test
+    collection = db[dbname]
+    collection.insert_many(data)
+
+
+if __name__ == '__main__':
+    urls = ["https://www.hamburg-airport.de/service/flightdata/arrivals",
+            "https://www.hamburg-airport.de/service/flightdata/departures"]
+
+    for url in urls:
+        flight_data = get_data(url)
+        save_data(flight_data, f"flights_{url.split('/')[-1]}")
